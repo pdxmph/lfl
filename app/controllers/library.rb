4dd2ec048cdfb8a14b8be1eaad67d83ee0b61e30
Michael Hall <michael.hall@puppetlabs.com>
2013-03-08 20:19:31 -0800

    Adding a simple inventory sheet

--- app/controllers/library.rb
+++ app/controllers/library.rb
@@ -6,6 +6,13 @@ Lfl.controllers :library do
     render 'libraries/inventory'
   end
 
+  get '/:id/inventory_sheet' do
+    @library =Library.find(params[:id])
+    @books = @library.books.find(:all, :order => "title ASC", :conditions => ["in_stock = ?",1])
+    render 'libraries/inventory_sheet', :layout => 'simple'
+  end
+
+
   get :index do 
     @libraries = Library.all
     render 'libraries/index'
