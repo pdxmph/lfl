ea3b61276892cebc56c73c73947e02e8138b7605
Michael Hall <michael.hall@puppetlabs.com>
2013-03-16 13:30:19 -0700

    Update to library controller

--- app/controllers/library.rb
+++ app/controllers/library.rb
@@ -1,17 +1,23 @@
 Lfl.controllers :library do
 
-  get :index, :with => :id do
+  get :index, :provides => [:html, :atom], :with => :id do
     @library =Library.find(params[:id])
-    @books = @library.books.find(:all, :order => "title ASC", :conditions => ["in_stock = ?",1])
+    @books = @library.books.find(:all, :order => "title ASC", :conditions => ["in_stock = ?",true])
     render 'libraries/inventory'
   end
 
   get '/:id/inventory_sheet' do
     @library =Library.find(params[:id])
-    @books = @library.books.find(:all, :order => "title ASC", :conditions => ["in_stock = ?",1])
+    @books = @library.books.find(:all, :order => "title ASC", :conditions => ["in_stock = ?",true])
     render 'libraries/inventory_sheet', :layout => 'simple'
   end
 
+
+  get '/:id/feed' do
+    @library = Library.find(params[:id])
+    @books = @library.books.find(:all, :order => "created_at DESC", :conditions => ["in_stock =?",true])
+    render 'libraries/atom'
+  end
 
   get :index do 
     @libraries = Library.all
