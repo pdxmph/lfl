679736653f9c6ac4cc98a8108df0cf9bc89d9218
Michael Hall <michael.hall@puppetlabs.com>
2013-03-24 12:08:27 -0700

    Making default statuses for new books (in, today's date for added)

--- models/book.rb
+++ models/book.rb
@@ -1,3 +1,11 @@
 class Book < ActiveRecord::Base
   belongs_to :library
+
+  after_initialize :init
+
+    def init
+      self.in_stock  ||= true           #will set the default value only if it's nil
+      self.added ||= Date.today #let's you set a default association
+    end
+
 end
