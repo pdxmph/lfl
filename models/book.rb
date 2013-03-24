8ff14c1170176a14dfdfb7d9033215efd35278ca
Michael Hall <michael.hall@puppetlabs.com>
2013-03-24 13:37:24 -0700

    Adding some stats and book status icons to inventory display

--- models/book.rb
+++ models/book.rb
@@ -1,11 +1,12 @@
 class Book < ActiveRecord::Base
   belongs_to :library
-
   after_initialize :init
 
     def init
-      self.in_stock  ||= true           #will set the default value only if it's nil
-      self.added ||= Date.today #let's you set a default association
+      self.in_stock  ||= true          
+      self.added ||= Date.today 
     end
+
+    scope :in, where(:in_stock => true)
 
 end
